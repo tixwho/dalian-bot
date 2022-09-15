de6c6dcaca5bd299cd9099057b929e71374f77ce
tixwho <905002146tix@gmail.com>
2022-09-15 16:36:30 +0800

    preliminary c & r examples.

--- internal/pkg/commands/crud.go
+++ internal/pkg/commands/crud.go
@@ -2,8 +2,14 @@ package commands
 
 import (
 	"dalian-bot/internal/pkg/clients"
+	"dalian-bot/internal/pkg/data"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/net/context"
+	"time"
 )
 
 type CrudCommand struct {
@@ -25,6 +31,13 @@ func (c *CrudCommand) New() {
 		MEGroup:          []string{"o"},
 	})
 	c.RegisterCommandFlag(CommandFlag{
+		Name:             "read",
+		FlagPrefix:       []string{"r", "read"},
+		RequiresExtraArg: false,
+		MultipleExtraArg: false,
+		MEGroup:          []string{"o"},
+	})
+	c.RegisterCommandFlag(CommandFlag{
 		Name:             "delete",
 		FlagPrefix:       []string{"d", "delete"},
 		RequiresExtraArg: false,
@@ -40,6 +53,24 @@ func (c *CrudCommand) New() {
 	})
 }
 
+type CrudTestStruct struct {
+	MsgID        string
+	ChannelID    string
+	MsgInfo      string
+	AuthorID     string
+	MsgTimestamp time.Time
+}
+
+func newTestStruct(createEvent *discordgo.MessageCreate) CrudTestStruct {
+	return CrudTestStruct{
+		MsgID:        createEvent.ID,
+		ChannelID:    createEvent.ChannelID,
+		MsgInfo:      createEvent.Content,
+		AuthorID:     createEvent.Author.ID,
+		MsgTimestamp: createEvent.Timestamp,
+	}
+}
+
 func (c *CrudCommand) Match(a ...any) bool {
 	m, isMsgCreate := a[0].(*discordgo.MessageCreate)
 	if !isMsgCreate {
@@ -51,6 +82,7 @@ func (c *CrudCommand) Match(a ...any) bool {
 
 func (c *CrudCommand) Do(a ...any) error {
 	m := a[0].(*discordgo.MessageCreate)
+	/* Handle Flags */
 	if err := c.ParseFlags(m.Message.Content); err != nil {
 		fmt.Println(err)
 		return err
@@ -58,6 +90,27 @@ func (c *CrudCommand) Do(a ...any) error {
 	if err := c.ValidateFlagMap(); err != nil {
 		clients.DgSession.ChannelMessageSend(m.ChannelID, err.Error())
 		return err
+	}
+	/* Flag parsed without error. Now follows various actions. */
+	if _, ok := c.FlagArgstatMaps["create"]; ok {
+		data.GetCollection("test_crud").InsertOne(context.TODO(), newTestStruct(m))
+		//debug
+		fmt.Println("Inserted message: " + m.ID)
+	} else if _, ok := c.FlagArgstatMaps["read"]; ok {
+		var singleResult *mongo.SingleResult
+		if singleResult = data.GetCollection("test_crud").FindOne(context.TODO(), bson.M{"authorid": m.Author.ID}); singleResult.Err() != nil {
+			if singleResult.Err() == mongo.ErrNoDocuments {
+				clients.DgSession.ChannelMessageSend(m.ChannelID, "Unable to find any testStruct for the given userID!")
+			}
+			fmt.Println("Unable to find any doc for authorid:" + m.Author.ID)
+			return errors.Wrap(singleResult.Err(), "Doc query failed")
+		}
+		var doc CrudTestStruct
+		if err := singleResult.Decode(&doc); err != nil {
+			fmt.Println("Unable to Decode doc into TestCrudStruct")
+			return errors.Wrap(err, "Decode doc failed")
+		}
+		clients.DgSession.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Found a Doc:%v", doc))
 	}
 	clients.DgSession.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Successfully read arguments w/ flag! \r %v", c.FlagArgstatMaps))
 	return nil
