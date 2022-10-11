34e44c07aa74e4aada689dc2226ec65f405457a7
tixwho <905002146tix@gmail.com>
2022-10-11 12:23:46 +0800

    Annotations for wrapper functions.

--- internal/pkg/discord/service.go
+++ internal/pkg/discord/service.go
@@ -14,23 +14,27 @@ const (
 	EmbedColorDanger   = 0xfa3838
 )
 
-// ChannelMessageSend a wrapper of discordgo ChannelMessageSend function
+// ChannelMessageSend A wrapper of discordgo ChannelMessageSend function.
 func ChannelMessageSend(channelID, content string) (*discordgo.Message, error) {
 	return clients.DgSession.ChannelMessageSend(channelID, content)
 }
 
+// ChannelMessageSendEmbed A wrapper of discordgo ChannelMessageSendEmbed function.
 func ChannelMessageSendEmbed(channelID string, embed *discordgo.MessageEmbed) (*discordgo.Message, error) {
 	return clients.DgSession.ChannelMessageSendEmbed(channelID, embed)
 }
 
-func ChannelReportError(channelID string, error error) (*discordgo.Message, error) {
+// ChannelMessageReportError Report the error as a plain message to given gild channel.
+func ChannelMessageReportError(channelID string, error error) (*discordgo.Message, error) {
 	return ChannelMessageSend(channelID, error.Error())
 }
 
+// InteractionRespondComplex Basic wrapper for discordgo.InteractionRespond.
 func InteractionRespondComplex(i *discordgo.Interaction, resp *discordgo.InteractionResponse) error {
 	return clients.DgSession.InteractionRespond(i, resp)
 }
 
+// InteractionRespondEmbed Shortcut method for fast reply including a MessageEmbed.
 func InteractionRespondEmbed(i *discordgo.Interaction, embed *discordgo.MessageEmbed) error {
 	return InteractionRespondComplex(i, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
@@ -40,6 +44,7 @@ func InteractionRespondEmbed(i *discordgo.Interaction, embed *discordgo.MessageE
 	})
 }
 
+// InteractionRespond Shortcut method for a simple message reply.
 func InteractionRespond(i *discordgo.Interaction, content string) error {
 	return InteractionRespondComplex(i, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
