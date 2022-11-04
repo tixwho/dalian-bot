642e14c09e962c21b5920a8f2ffcc7ff9ba19804
tixwho <905002146tix@gmail.com>
2022-11-05 00:41:56 +0800

    add more discord supportive methods for Interactions

--- internal/pkg/discord/service.go
+++ internal/pkg/discord/service.go
@@ -54,6 +54,27 @@ func InteractionRespond(i *discordgo.Interaction, content string) error {
 	})
 }
 
+func InteractionResponse(i *discordgo.Interaction) (*discordgo.Message, error) {
+	return clients.DgSession.InteractionResponse(i)
+}
+
+func InteractionResponseEdit(i *discordgo.Interaction, newresp *discordgo.WebhookEdit) (*discordgo.Message, error) {
+	return clients.DgSession.InteractionResponseEdit(i, newresp)
+}
+
+func InteractionResponseEditFromMessage(i *discordgo.Interaction, msg *discordgo.Message) (*discordgo.Message, error) {
+	tempWebhookEdit := &discordgo.WebhookEdit{
+		Content:    &msg.Content,
+		Components: &msg.Components,
+		Embeds:     &msg.Embeds,
+		//files is not compatible with message. use a new message for edited file.
+		//Files:         nil,
+		//the same goes AllowedMentions
+		//AllowedMentions: nil ,
+	}
+	return InteractionResponseEdit(i, tempWebhookEdit)
+}
+
 // ChannelFileSend send a file to given guild channel.
 // channelID the id of a channel
 // name the display filename to be sent to discord
