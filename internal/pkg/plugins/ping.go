b13010253b661973bf34fe22dca27ac1100ea67f
tixwho <905002146tix@gmail.com>
2022-12-23 20:01:50 +0800

    Plugins for Dalian 2.0

---
+++ internal/pkg/plugins/ping.go
@@ -0,0 +1,97 @@
+package plugins
+
+import (
+	"dalian-bot/internal/pkg/core"
+	"dalian-bot/internal/pkg/services/discord"
+	"errors"
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+)
+
+type PingPlugin struct {
+	core.Plugin
+	DiscordService *discord.Service
+	core.StartWithMatchUtil
+	discord.SlashCommand
+	discord.IDisrocdHelper
+}
+
+func (p *PingPlugin) DoNamedInteraction(_ *core.Bot, i *discordgo.InteractionCreate) (err error) {
+	if match, name := p.DefaultMatchCommand(i); match {
+		switch name {
+		case "ping":
+			p.DiscordService.ChannelMessageSend(i.ChannelID, "pong response not using interaction!")
+			p.DiscordService.InteractionRespondComplex(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "pong response with discord interaction!!",
+				},
+			})
+		}
+	}
+	return nil
+}
+
+func (p *PingPlugin) DoMessage(_ *core.Bot, m *discordgo.MessageCreate) error {
+	if matched, _ := p.StartWithMatchUtil.MatchText(m.Content, p.DiscordService.DiscordAccountConfig); matched {
+		p.DiscordService.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+	return nil
+}
+
+func (p *PingPlugin) Init(reg *core.ServiceRegistry) error {
+	//discordService is a MUST have. return error if not found.
+	if err := reg.FetchService(&p.DiscordService); err != nil {
+		return err
+	}
+
+	p.AcceptedTriggerTypes = []core.TriggerType{core.TriggerTypeDiscord}
+	p.Name = "ping"
+	p.Identifiers = []string{"ping"}
+	p.AppCommandsMap = make(map[string]*discordgo.ApplicationCommand)
+	p.AppCommandsMap.RegisterCommand(&discordgo.ApplicationCommand{
+		Name:        "ping",
+		Description: "Ping command for Dalian",
+	})
+	p.DiscordService.RegisterSlashCommand(p)
+
+	formattedPingHelp := fmt.Sprintf("*Call*: /ping,%sping\rrespond a \"pong\"", p.DiscordService.DiscordAccountConfig.Prefix)
+	p.IDisrocdHelper = discord.GenerateHelper(discord.HelperConfig{
+		PluginHelp: "Basic ping command for dalian over Discord.",
+		CommandHelps: []discord.CommandHelp{
+			{
+				Name:          "ping",
+				FormattedHelp: formattedPingHelp,
+			},
+		},
+	})
+
+	return nil
+}
+
+func (p *PingPlugin) Trigger(trigger core.Trigger) {
+	if !p.AcceptTrigger(trigger.Type) {
+		return
+	}
+	discordEvent := discord.UnboxEvent(trigger) // not checking because only accept discord.
+	switch discordEvent.EventType {
+	case discord.EventTypeMessageCreate:
+		if p.DiscordService.IsGuildMessageFromBotOrSelf(discordEvent.MessageCreate.Message) {
+			return
+		}
+		p.DoMessage(trigger.Bot, discordEvent.MessageCreate)
+	case discord.EventTypeInteractionCreate:
+		p.DoNamedInteraction(trigger.Bot, discordEvent.InteractionCreate)
+	default:
+		core.Logger.Warnf("This should NOT reach!")
+	}
+}
+
+func NewPingPlugin(reg *core.ServiceRegistry) core.INewPlugin {
+	var ping PingPlugin
+	if err := (&ping).Init(reg); err != nil && errors.As(err, &core.ErrServiceFetchUnknownService) {
+		core.Logger.Panicf("Ping plugin MUST have all required service(s) injected!")
+		panic("Ping plugin initialization failed.")
+	}
+	return &ping
+}
---
+++ internal/pkg/plugins/what.go
@@ -0,0 +1,76 @@
+package plugins
+
+import (
+	"dalian-bot/internal/pkg/core"
+	"dalian-bot/internal/pkg/services/discord"
+	"errors"
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"regexp"
+)
+
+type WhatPlugin struct {
+	core.Plugin
+	DiscordService *discord.Service
+	core.RegexMatchUtil
+}
+
+func (p *WhatPlugin) DoMessage(_ *core.Bot, m *discordgo.MessageCreate) (err error) {
+	matchStatus, _ := p.RegMatchMessage(m.Content)
+	//doing `what`.
+	if matchStatus {
+		step := 2
+		for {
+			msgs, err := p.DiscordService.Session.ChannelMessages(m.ChannelID, step, m.ID, "", "")
+			if err != nil {
+				core.Logger.Warn("Error getting %d message prior to || %s", step, m.Content)
+				return err
+			}
+			msg, foundNonBot := discord.FindFirstNonBotMsg(msgs)
+			if foundNonBot {
+				p.DiscordService.Session.ChannelMessageSend(msg.ChannelID, fmt.Sprintf("**%s**", msg.Content))
+				return nil
+			}
+			step *= 2
+		}
+	}
+	return nil
+}
+
+func (p *WhatPlugin) Init(reg *core.ServiceRegistry) error {
+	if err := reg.FetchService(&p.DiscordService); err != nil {
+		return err
+	}
+
+	p.AcceptedTriggerTypes = []core.TriggerType{core.TriggerTypeDiscord}
+	p.Name = "what"
+	p.RegexExpressions = []*regexp.Regexp{regexp.MustCompile("^what$")}
+	return nil
+}
+
+func (p *WhatPlugin) Trigger(trigger core.Trigger) {
+	if !p.AcceptTrigger(trigger.Type) {
+		return
+	}
+	// only accept discord so far, so not using switch.
+	// example of accepting other triggers can be found at:
+	discordEvent := discord.UnboxEvent(trigger)
+	switch discordEvent.EventType {
+	case discord.EventTypeMessageCreate:
+		if p.DiscordService.IsGuildMessageFromBotOrSelf(discordEvent.MessageCreate.Message) {
+			return
+		}
+		p.DoMessage(trigger.Bot, discordEvent.MessageCreate)
+	default:
+		//not handling any other type of discordEvent.
+		return
+	}
+}
+
+func NewWhatPlugin(reg *core.ServiceRegistry) core.INewPlugin {
+	var what WhatPlugin
+	if err := (&what).Init(reg); err != nil && errors.As(err, &core.ErrServiceFetchUnknownService) {
+		core.Logger.Panicf("What plugin MUST have all required service(s) injected!")
+	}
+	return &what
+}
