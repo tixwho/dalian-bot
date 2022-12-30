01b65f3047c28dc2a22727a202115789ec0e6fec
tixwho <905002146tix@gmail.com>
2022-12-30 12:18:01 +0800

    Mongo database wrapper functions.

--- internal/pkg/services/data/service.go
+++ internal/pkg/services/data/service.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"dalian-bot/internal/pkg/core"
+	"errors"
 	"fmt"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -51,6 +52,48 @@ func (s *Service) Status() error {
 
 func (s *Service) GetCollection(name string, opts ...*options.CollectionOptions) *mongo.Collection {
 	return s.Client.Database("dalian").Collection(name, opts...)
+}
+
+func (s *Service) Find(receiver any, collection *mongo.Collection, ctx context.Context, filter any, options ...*options.FindOptions) error {
+	if reflect.TypeOf(receiver).Kind() != reflect.Ptr {
+		return errors.New("receiver is not a pointer")
+	}
+	findCursor, err := collection.Find(ctx, filter, options...)
+	if err != nil {
+		core.Logger.Warnf("Database cursor error: %v", err)
+		return err
+	}
+	if err = findCursor.All(context.TODO(), receiver); err != nil {
+		core.Logger.Warnf("Database unmarshal error: %v", err)
+		return err
+	}
+	return nil
+}
+
+func (s *Service) FindOne(receiver any, collection *mongo.Collection, ctx context.Context, filter any,
+	options ...*options.FindOneOptions) error {
+	if reflect.TypeOf(receiver).Kind() != reflect.Ptr {
+		return errors.New("receiver is not a pointer")
+	}
+	findOneResult := collection.FindOne(ctx, filter, options...)
+	if findOneResult.Err() != nil {
+		// can be mongo.ErrNoDocuments or other type of error
+		return findOneResult.Err()
+	}
+	if err := findOneResult.Decode(receiver); err != nil {
+		core.Logger.Warnf("Database unmarshal error: %v", err)
+		return err
+	}
+	return nil
+
+}
+
+func (s *Service) InsertOne(subject any, collection *mongo.Collection, ctx context.Context, options ...*options.InsertOneOptions) error {
+	_, err := collection.InsertOne(ctx, subject, options...)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 type ServiceConfig struct {
