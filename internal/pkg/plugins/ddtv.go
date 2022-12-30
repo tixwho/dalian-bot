5c8ab3c33497c2d41e905de6da1b14f6efc069b7
tixwho <905002146tix@gmail.com>
2022-12-30 16:31:35 +0800

    Now using SubCommandGroup for command organization.

--- internal/pkg/plugins/ddtv.go
+++ internal/pkg/plugins/ddtv.go
@@ -1,12 +1,18 @@
 package plugins
 
 import (
+	"context"
 	"dalian-bot/internal/pkg/core"
 	"dalian-bot/internal/pkg/services/data"
 	"dalian-bot/internal/pkg/services/ddtv"
 	"dalian-bot/internal/pkg/services/discord"
 	"errors"
+	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type DDTVPlugin struct {
@@ -17,13 +23,41 @@ type DDTVPlugin struct {
 	discord.IDisrocdHelper
 }
 
-const (
-	NotifChannelID = "920708761600028713" // todo: use database instead
-)
+func (p *DDTVPlugin) DoNamedInteraction(b *core.Bot, i *discordgo.InteractionCreate) (e error) {
+	if isMatched, cmdName := p.DefaultMatchCommand(i); !isMatched {
+		fmt.Printf("nothing matched: %v", i)
+		return nil
+	} else {
+		switch cmdName {
+		case "ddtv":
+			cmdOptions := i.ApplicationCommandData().Options
+			switch cmdOptions[0].Name {
+			case "webhook-channel":
+				cmdOptions := cmdOptions[0].Options
+				switch cmdOptions[0].Name {
+				case "set":
+					updateResult, err := p.upsertOneWebhookNotifyChannel(ddtvNotifyPo{
+						AdminDiscordUserID: i.Interaction.Member.User.ID,
+						GuildID:            i.Interaction.GuildID,
+						NotifyChannelID:    i.Interaction.ChannelID,
+					})
+					if err != nil {
+						core.Logger.Warnf("Error inserting notification channel record: %v", err)
+						return err
+					}
+					if updateResult.UpsertedCount > 0 {
+						p.DiscordService.InteractionRespond(i.Interaction, "notification channel created!")
+					} else {
+						p.DiscordService.InteractionRespond(i.Interaction, "already a notification channel!")
+					}
+				case "remove":
+					// todo: add remove.
+				}
+			}
+		}
+	}
 
-func (p *DDTVPlugin) DoNamedInteraction(b *core.Bot, i *discordgo.InteractionCreate) (err error) {
-	//TODO implement me
-	panic("implement me")
+	return nil
 }
 
 func (p *DDTVPlugin) Init(reg *core.ServiceRegistry) error {
@@ -41,40 +75,32 @@ func (p *DDTVPlugin) Init(reg *core.ServiceRegistry) error {
 	p.Name = "ddtv"
 	p.AppCommandsMap = make(map[string]*discordgo.ApplicationCommand)
 	p.AppCommandsMap.RegisterCommand(&discordgo.ApplicationCommand{
-		Name:        "ddtv-set-notification-channel",
-		Description: "Set this channel as a DDTV webhook notification channel.",
-		/* No access-token validation for now
+		Name:        "ddtv",
+		Description: "ddtv commands",
 		Options: []*discordgo.ApplicationCommandOption{
 			{
-				Type: discordgo.ApplicationCommandOptionString,
-				Name: "command-name",
-				//late init, replace %s with separator
-				Description: "Name of the command.",
-				Required:    false,
+				Name:        "webhook-channel",
+				Description: "webhook channel commands",
+				Type:        discordgo.ApplicationCommandOptionSubCommandGroup,
+				Options: []*discordgo.ApplicationCommandOption{
+					{
+						Name:        "set",
+						Type:        discordgo.ApplicationCommandOptionSubCommand,
+						Description: "Set current channel as ddtv webhook channel",
+					}, {
+						Name:        "remove",
+						Type:        discordgo.ApplicationCommandOptionSubCommand,
+						Description: "Remove current channel as ddtv webhook channel",
+					},
+				},
 			},
 		},
-		*/
-	})
-	p.AppCommandsMap.RegisterCommand(&discordgo.ApplicationCommand{
-		Name:        "ddtv-remove-notification-channel",
-		Description: "Remove this channel from DDTV webhook notification channel list.",
-		/* No access-token validation for now
-		Options: []*discordgo.ApplicationCommandOption{
-			{
-				Type: discordgo.ApplicationCommandOptionString,
-				Name: "command-name",
-				//late init, replace %s with separator
-				Description: "Name of the command.",
-				Required:    false,
-			},
-		},
-		*/
 	})
 
-	formattedHelpSetNotifChannel := `*DDTV set-notification-channel*: /DDTV set-notification-channel
+	formattedHelpSetNotifChannel := `*ddtv webhook-channel set*: /ddtv webhook-channel set
 Set current channel as a DDTV webhook notification channel.
 Dalian will send a message for every incoming DDTV webhook received in this channel`
-	formattedHelpRemoveNotifChannel := `*DDTV remove-notification-channel*: /DDTV set-notification-channel
+	formattedHelpRemoveNotifChannel := `ddtv webhook-channel remove*: /ddtv webhook-channel remove
 Remove current channel from DDTV webhook notification channel list.
 Dalian will no longer send a message for every incoming DDTV webhook received in this channel`
 
@@ -82,11 +108,11 @@ Dalian will no longer send a message for every incoming DDTV webhook received in
 		PluginHelp: "Helper support for Dalian.",
 		CommandHelps: []discord.CommandHelp{
 			{
-				Name:          "ddtv-set-notification-channel",
+				Name:          "ddtv webhook-channel set",
 				FormattedHelp: formattedHelpSetNotifChannel,
 			},
 			{
-				Name:          "ddtv-remove-notification-channel",
+				Name:          "ddtv webhook-channel remove",
 				FormattedHelp: formattedHelpRemoveNotifChannel,
 			},
 		},
@@ -95,7 +121,6 @@ Dalian will no longer send a message for every incoming DDTV webhook received in
 }
 
 func (p *DDTVPlugin) Trigger(trigger core.Trigger) {
-	//TODO implement me
 	if !p.AcceptTrigger(trigger.Type) {
 		return
 	}
@@ -110,6 +135,7 @@ func (p *DDTVPlugin) Trigger(trigger core.Trigger) {
 			switch dcEvent.InteractionCreate.Type {
 			case discordgo.InteractionApplicationCommand:
 				// slash command
+				p.DoNamedInteraction(trigger.Bot, dcEvent.InteractionCreate)
 			default:
 				// not accepting other type of interaction for this plugin
 				return
@@ -121,19 +147,59 @@ func (p *DDTVPlugin) Trigger(trigger core.Trigger) {
 	case core.TriggerTypeDDTV:
 		// do ddtv webhook thing
 		ddtvEvent := ddtv.UnboxEvent(trigger)
-		p.notifyDDTVWebhookToChannel(NotifChannelID, ddtvEvent.WebHook) // todo: use database instead
+		p.notifyDDTVWebhookToChannels(ddtvEvent.WebHook)
 	default:
 		core.Logger.Warnf(core.LogPromptUnknownTrigger, trigger.Type)
 	}
 }
 
-func (p *DDTVPlugin) notifyDDTVWebhookToChannel(channelID string, webhook ddtv.WebHook) {
-	_, err := p.DiscordService.ChannelMessageSendEmbed(channelID, webhook.DigestEmbed())
+func (p *DDTVPlugin) notifyDDTVWebhookToChannels(webhook ddtv.WebHook) {
+	channelIDs, err := p.findDDTVWebhookNotifyChannels()
 	if err != nil {
-		core.Logger.Warnf("Embed sent failed: %v", err)
+		core.Logger.Warnf("Retrieve webhook channels failed!: %v", err)
 		return
 	}
-	core.Logger.Debugf("Webhook Info Sent to Channel [%s]", channelID) // todo: remove.
+	for _, channelID := range channelIDs {
+		_, err := p.DiscordService.ChannelMessageSendEmbed(channelID, webhook.DigestEmbed())
+		if err != nil {
+			core.Logger.Warnf("Embed sent failed: %v", err)
+			return
+		}
+		core.Logger.Debugf("Webhook Info Sent to Channel [%s]", channelID) // todo: remove.
+	}
+
+}
+
+type ddtvNotifyPo struct {
+	BsonID             primitive.ObjectID `bson:"_id,omitempty"`
+	AdminDiscordUserID string             `bson:"admin_dc_user_id"`
+	GuildID            string             `bson:"guild_id"`
+	NotifyChannelID    string             `bson:"notify_channel_id"`
+}
+
+func (p *DDTVPlugin) getCollection() *mongo.Collection {
+	return p.DataService.GetCollection("ddtv_notify_channels")
+}
+
+func (p *DDTVPlugin) upsertOneWebhookNotifyChannel(po ddtvNotifyPo) (*mongo.UpdateResult, error) {
+	rawResult := p.DataService.UpdateOne(bson.D{{"$set", data.ToBsonDocForce(po)}}, p.getCollection(), context.Background(), bson.M{"notify_channel_id": po.NotifyChannelID}, options.Update().SetUpsert(true))
+	if rawResult.Err() != nil {
+		return nil, rawResult.Err()
+	}
+	return rawResult.UpdateResult(), rawResult.Err()
+}
+
+func (p *DDTVPlugin) findDDTVWebhookNotifyChannels() (channels []string, er error) {
+	var results []ddtvNotifyPo
+	if err := p.DataService.Find(&results, p.getCollection(), context.Background(), bson.M{}); err != nil {
+		return nil, err
+	}
+	var channelIDs []string
+	for _, v := range results {
+		channelIDs = append(channelIDs, v.NotifyChannelID)
+	}
+	return channelIDs, nil
+
 }
 
 func NewDDTVPlugin(reg *core.ServiceRegistry) core.INewPlugin {
