f21b9317d32315158160a757558d094f1b4dff9f
tixwho <905002146tix@gmail.com>
2023-02-18 04:03:18 +0800

    shell for jenkins v6

--- shell/start.sh
+++ shell/start.sh
@@ -1,4 +1,5 @@
 current_path=$(realpath .)
 echo "Current Path: ${current_path}"
 cd "apps/dalian/dalian-deploy"
-screen -dmS dalian dalian-next
+screen -dmS dalian ./dalian-next
+echo "Dalian is now ONLINE."
